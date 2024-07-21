8649e01823680f889987f43fc1731a62a4ceb291
Anatolii Halas <tolikgalas@gmail.com>
2024-07-21 05:36:28 +0300

    Fixes

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,19 +1,10 @@
 import { MyComponents } from "@portfolio/ui";
 import Head from "next/head";
 
-export default function Home({
-  title,
-}: {
-  title: string;
-}) {
+export default function Home({ title }: { title: string }) {
   return (
-    <>
-      <Head>
-        <title>{title}</title>
-      </Head>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <MyComponents />
-      </main>
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <MyComponents />
+    </main>
   );
 }
--- packages/ui/src/apps/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Page() {
-    return (
-        <div>My page</div>
-    );
-}
