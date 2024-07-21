fc5a7fe62cde0e20e30daa08303306c9363bd95c
Anatolii Halas <tolikgalas@gmail.com>
2024-07-21 16:58:06 +0300

    Fixed ci/ test & lint jobs

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,15 +1,9 @@
 import { MyComponents } from "@portfolio/ui";
-import Head from "next/head";
 
-export default function Home({ title }: { title: string }) {
+export default function Home() {
   return (
-    <>
-      <Head>
-        <title>{title}</title>
-      </Head>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <MyComponents />
-      </main>
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <MyComponents />
+    </main>
   );
 }
