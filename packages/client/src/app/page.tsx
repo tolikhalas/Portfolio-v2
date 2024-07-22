fa940004d4fe6c7ad05dad015fc3180ac94d76b9
Anatolii Halas <tolikgalas@gmail.com>
2024-07-23 02:21:14 +0300

    Updated Home page in client package

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,5 +1,8 @@
 import { MyComponents } from "@portfolio/ui";
 
+/**
+ * Home Page
+ */
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
